5ce86c505675d16028e4d59bba904356cb624438
Pepakayala V Siva Ganesh Naga Sai Kumar <157547445+sivaganesh7@users.noreply.github.com>
2026-01-01 18:19:09 +0530

    update code in freq_count.java

--- Arrays/freq_count.java
+++ Arrays/freq_count.java
@@ -2,7 +2,6 @@ package Arrays;
 
 public class freq_count {
     public static void main(String[] args) {
-        
     int[] arr = {1, 2, 2, 3, 3, 3, 4};
     for (int i = 0; i < arr.length; i++) {
     int count = 1;
